545f0238cb12d707bc5caf1463c962c73ff93c7a
daman0826 <daman0826@users.noreply.github.com>
2016-11-16 15:16:54 -0500

    Create Energy.java

---
+++ core/src/io/github/bobjoetom/craftingwar/cards/Energy.java
@@ -0,0 +1,29 @@
+package io.github.bobjoetom.craftingwar.cards;
+
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Created by Brenden on 4/12/2016.
+ */
+public class Energy extends Card {
+
+    public Energy() {
+        super();
+        setID("Energy");
+        setPoints(5);
+        setCardTexturePath("energy.png");
+        setCardSelectedTexturePath("energy-Selected.png");
+        setRenderedCard(getCardTexturePath());
+
+    }
+    public boolean isCombinable(Card e) {
+        return e.getID() == "Energy";
+    }
+    @Override
+    public Card combine(Card e) {
+        if(e.getID()=="Energy"){
+            return new Energy_Boost();
+        }
+        return new Card();
+    }
+}
