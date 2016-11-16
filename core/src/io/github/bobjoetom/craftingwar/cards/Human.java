2bf5b3ad35b3f3ecf5375f4839699e5da291b316
LimeDog <echindev@gmail.com>
2016-11-16 15:17:50 -0500

    Create Human.java

---
+++ core/src/io/github/bobjoetom/craftingwar/cards/Human.java
@@ -0,0 +1,29 @@
+package io.github.bobjoetom.craftingwar.cards;
+
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Created by Emerson Chin on 11/16/2016.
+ */
+public class Human extends Card {
+
+    public Human() {
+        super();
+        setID("Human");
+        setPoints(5);
+        setCardTexturePath("human.png");
+        setCardSelectedTexturePath("human-Selected.png");
+        setRenderedCard(getCardTexturePath());
+
+    }
+    public boolean isCombinable(Card e) {
+        return e.getID() == "Human";
+    }
+    @Override
+    public Card combine(Card e) {
+        if(e.getID()=="Human"){
+            return new Human();
+        }
+        return new Card();
+    }
+}
